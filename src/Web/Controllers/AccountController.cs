479f209f66d1db51d08b0a44d281693fa8dcd703
Tyler Lu <admin@tyler.lu>
2018-03-07 17:13:50 +0800

    Update AccountController.cs

--- src/Web/Controllers/AccountController.cs
+++ src/Web/Controllers/AccountController.cs
@@ -1,4 +1,4 @@
-﻿using ApplicationCore.Interfaces;
+using ApplicationCore.Interfaces;
 using Infrastructure.Identity;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
@@ -53,40 +53,32 @@ namespace Microsoft.eShopWeb.Controllers
         // POST: /Account/SignIn
         [HttpPost]
         [AllowAnonymous]
-        //[ValidateAntiForgeryToken]
+        [ValidateAntiForgeryToken]
         public async Task<IActionResult> SignIn(LoginViewModel model, string returnUrl = null)
         {
-            try
+            if (!ModelState.IsValid)
             {
-                if (!ModelState.IsValid)
-                {
-                    return View(model);
-                }
-                ViewData["ReturnUrl"] = returnUrl;
-
-                var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, lockoutOnFailure: false);
-                if (result.RequiresTwoFactor)
-                {
-                    return RedirectToAction(nameof(LoginWith2fa), new { returnUrl, model.RememberMe });
-                }
-                if (result.Succeeded)
-                {
-                    string anonymousBasketId = Request.Cookies[Constants.BASKET_COOKIENAME];
-                    if (!String.IsNullOrEmpty(anonymousBasketId))
-                    {
-                        await _basketService.TransferBasketAsync(anonymousBasketId, model.Email);
-                        Response.Cookies.Delete(Constants.BASKET_COOKIENAME);
-                    }
-                    return RedirectToLocal(returnUrl);
-                }
-                ModelState.AddModelError(string.Empty, "Invalid login attempt.");
                 return View(model);
+            }
+            ViewData["ReturnUrl"] = returnUrl;
 
-            }
-            catch (Exception ex)
+            var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, lockoutOnFailure: false);
+            if (result.RequiresTwoFactor)
             {
-                return Content(ex.Message + "\r\n" + ex.Source);
+                return RedirectToAction(nameof(LoginWith2fa), new { returnUrl, model.RememberMe });
             }
+            if (result.Succeeded)
+            {
+                string anonymousBasketId = Request.Cookies[Constants.BASKET_COOKIENAME];
+                if (!String.IsNullOrEmpty(anonymousBasketId))
+                {
+                    await _basketService.TransferBasketAsync(anonymousBasketId, model.Email);
+                    Response.Cookies.Delete(Constants.BASKET_COOKIENAME);
+                }
+                return RedirectToLocal(returnUrl);
+            }
+            ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+            return View(model);
         }
 
         [HttpGet]
